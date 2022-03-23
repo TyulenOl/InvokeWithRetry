9bc0d089d8505791ac9023b754822f59157a89d5
TyulenOl <setu787898@gmail.com>
2022-03-23 21:43:37 +0500

    Активность «InvokeWithRetry»

---
+++ InvokeWithRetry/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InvokeWithRetry
+{
+    public static class Program
+    {
+        static void Main()
+        {
+        
+        }
+        
+        public static bool InvokeWithRetry(Action action, int maxAttempts)
+        {
+            for (var i = 0; i < maxAttempts; i++)
+            {
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception)
+                {
+                    if (i == maxAttempts - 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TestsInvokeWithRetry/UnitTest1.cs
@@ -0,0 +1,39 @@
+using System;
+using InvokeWithRetry;
+using NUnit.Framework;
+
+namespace TestsInvokeWithRetry;
+
+public class Tests
+{
+    private static int attempts;
+    private static int attemptsExceptions;
+    private static int numberExceptionsThrown = 10;
+
+    private readonly Action countingAction = () => attempts++;
+
+    private readonly Action throwExceptionsAction = () =>
+    {
+        if (attemptsExceptions < numberExceptionsThrown)
+        {
+            attemptsExceptions++;
+            throw new Exception();
+        }
+    };
+
+    [Test]
+    public void TestWithThrowExceptionsAction()
+    {
+        var lessNumberExceptionsThrown = Program.InvokeWithRetry(throwExceptionsAction, 6);
+        var moreNumberExceptionsThrown = Program.InvokeWithRetry(throwExceptionsAction, 14);
+        Assert.AreEqual(false, lessNumberExceptionsThrown);
+        Assert.AreEqual(true, moreNumberExceptionsThrown);
+    }
+
+    [Test]
+    public void TestWithСountingAction()
+    {
+        Assert.AreEqual(true, Program.InvokeWithRetry(countingAction, 4));
+        Assert.AreEqual(4, attempts);
+    }
+}
